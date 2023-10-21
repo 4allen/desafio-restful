9612e30e08a6e6669f2b6a6d9dc6208b3333b437
hellen atanasio <l.atanasio@hotmail.com>
2023-10-21 14:43:29 -0300

    Initial commit

---
+++ src/test/java/me/hellen/desafio/DesafioRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hellen.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
